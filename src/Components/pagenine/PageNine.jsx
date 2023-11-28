e5c184ca240e5d51b51b1d4b9fc82209d1db17fb
Arathy <arathyp.radhakrishnan@gmail.com>
2023-11-28 17:30:56 +0530

    redappleblob

--- src/Components/pagenine/PageNine.jsx
+++ src/Components/pagenine/PageNine.jsx
@@ -1,15 +1,69 @@
-import React from "react";
-import "./PageNine.css";
+import React, { useState, useEffect } from "react";
 import pagenine from "../../assets/PageNine.png";
-import shape from "../../assets/shape.png";
-import launchingsoon from "../../assets/launchingsoon.png";
+import "./PageNine.css";
 
 const PageNine = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    const svgContainer = document.getElementById("svg-container");
+
+    const updateBlobPosition = () => {
+      const blob = document.getElementById("blob");
+      const { x, y } = mousePosition;
+
+      // Update the blob's position based on the mouse cursor
+      blob.setAttribute("cx", x);
+      blob.setAttribute("cy", y);
+    };
+
+    svgContainer.addEventListener("mousemove", handleMouseMove);
+
+    // Initial update
+    updateBlobPosition();
+
+    return () => {
+      svgContainer.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [mousePosition]);
+
   return (
-    <div className="h-screen  ">
-      <img src={pagenine} className="h-screen  w-screen " alt="" />
-      <img src={launchingsoon} className="h-screen  w-screen  " alt="" />
-    </div>
+    <body>
+      <div className="svgblob" id="svg-container">
+        <svg
+          viewBox="0 0 500 500"
+          preserveAspectRatio="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient id="blobgradient-1">
+              <stop offset="0%" stopColor="#e8e8e8"></stop>
+              <stop offset="100%" stopColor="#e8e8e8"></stop>
+            </linearGradient>
+          </defs>
+
+          {/* Mask with the blob shape */}
+          <mask id="blobmask-1">
+            <rect width="100%" height="100%" fill="#fff"></rect>
+            <circle id="blob" cx="0" cy="0" r="50" fill="#000"></circle>
+          </mask>
+
+          {/* Background rectangle */}
+          <rect
+            width="100%"
+            height="100%"
+            mask="url('#blobmask-1')"
+            fill="url('#blobgradient-1')"
+          ></rect>
+        </svg>
+
+        <img src={pagenine} alt="" />
+      </div>
+    </body>
   );
 };
 
--- src/Components/pagethree/PageThree.jsx
+++ src/Components/pagethree/PageThree.jsx
@@ -3,41 +3,36 @@ import curser from "../../assets/cursorPagethree.png";
 import "./PageThree.css";
 
 const PageThree = () => {
-  const preScroll = useRef(null);
   const elemRef = useRef(null);
-  const [scale, setScale] = useState(1.05);
+  const scrollTargetRef = useRef(null);
+  const [scale, setScale] = useState(1);
 
-  useLayoutEffect(() => {
-    const botPos = (element) => element.getBoundingClientRect().bottom;
-    const onScroll = () => {
-      const divBotPos = botPos(elemRef.current);
-      const scrollPos = preScroll.current > window.scrollY;
-      preScroll.current = window.scrollY;
-      if (scrollPos && divBotPos > window.innerHeight) {
-        setScale(1);
-        return;
-      }
-      if (divBotPos < window.innerHeight) {
-        setScale(10);
-      }
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = scrollTargetRef.current.scrollTop;
+      setScale(Math.min(1 + scrollY / 50, 10)); // You can adjust the divisor for a smoother or faster scaling effect
     };
 
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
+    const scrollTarget = scrollTargetRef.current;
+    scrollTarget.addEventListener("scroll", handleScroll);
+
+    return () => {
+      scrollTarget.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
   return (
     <div
       ref={elemRef}
-      className="flex justify-center  w-[100%] h-screen sm:flex-row vvsm:flex-col
-    "
+      className="flex justify-center  w-[100%] h-screen sm:flex-row vvsm:flex-col"
     >
       {/* first part */}
-      <div className="overflow-hidden flex justify-center items-center relative z-10  sm:w-[40%] sm:h-screen border-t-2 sm:border-r-2 sm:border-b-0 vsm:h-[300px] vvsm:h-[230px] vvsm:border-b-2 ">
+      <div className="overflow-hidden flex justify-center items-center relative z-10 sm:w-[40%] sm:h-screen border-t-2 sm:border-r-2 sm:border-b-0 vsm:h-[300px] vvsm:h-[230px] vvsm:border-b-2 ">
         <img
           src={curser}
           alt=""
           style={{
-            transition: "transform 1000ms ease-in-out",
+            transition: "transform 500ms ease-in-out",
             transform: `scale(${scale})`,
           }}
           className="absolute z-20 lg:w-[400px] lg:h-[400px] md:w-[300px] md:h-[300px] vsm:w-[250px] vsm:h-[250px] vvsm:w-[190px] vvsm:h-[190px] image-scaling "
@@ -58,22 +53,17 @@ const PageThree = () => {
 
       {/* second part */}
       <div className=" sm:w-[15%]  sm:h-screen flex flex-col justify-center items-center  sm:border-t-2 sm:border-r-2 vvsm:border-b-2 sm:border-b-0">
-        <div
-          className=" flex justify-center items-center sm:-rotate-90 font-Forum font-normal
-                        md:w-[500px] md:h-[80px] md:text-[60px]  sm:w-[400px] sm:h-[60px] sm:text-[50px] vvsm:w-[250px] vvsm:h-[65px] vvsm:text-[26px]"
-        >
+        <div className=" flex justify-center items-center sm:-rotate-90 font-Forum font-normal md:w-[500px] md:h-[80px] md:text-[60px]  sm:w-[400px] sm:h-[60px] sm:text-[50px] vvsm:w-[250px] vvsm:h-[65px] vvsm:text-[26px]">
           Services We Offer
         </div>
       </div>
 
       {/* third part */}
-
-      <div
-        // ref={thirdSectionContainerRef}
-        className="  overflow-auto  sm:w-[45%] sm:h-screen flex flex-col items-center justify-around sm:border-t-2 "
-      >
+      <div className="overflow-auto sm:w-[45%] sm:h-screen flex flex-col items-center justify-around sm:border-t-2 ">
         <div
-          className="overflow-y-scroll h-[65vh] ml-20  hide-scrollbar"
+          id="scrollTarget"
+          ref={scrollTargetRef}
+          className="overflow-y-scroll h-[65vh] ml-20 hide-scrollbar"
           style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
         >
           <div className=" sm:w-[60%] vvsm:w-[80%] flex flex-col  justify-center">
